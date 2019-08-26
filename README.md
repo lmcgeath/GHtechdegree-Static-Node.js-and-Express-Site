87066b0c4c0da95a00c0ee07f9a7cb148e0db465
Lucas McGeath <49741190+lmcgeath@users.noreply.github.com>
2019-08-26 12:10:50 -0700

    Final

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # GHtechdegree-Static Node.js and Express Site
  Project 6
-Added box shadows, background colors and shifted elements on the home page
+Added box shadows, background colors, transitions and shifted elements on the home page
--- app.js
+++ app.js
@@ -17,23 +17,19 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => { 
     res.render('about')
 });
-
-// Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized version of the Pug project template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
+// Add dynamic "project" routes based on the id of the project that render a customized version of the Pug project template to show off each project.
 app.get('/project/:id', (req, res) => {
     const id = req.params.id
     const project = projects[id]
-    res.render('project', {project})  
-    // res.locals = 'data.json'
-    // const project = projects[id]
-    
+    res.render('project', {project})    
 });
-
+//Creates error object if the user direct to a page that doesn't exist
 app.use((req, res, next) => {
     const err = new Error('Not Found')
     err.status = 404;
     next(err);
 })
-//For some reason having this commented out keeps the 'cannot set headers' error from being thrown when I make changes to the pug files...
+//Formats 404 error to be more readable 
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
