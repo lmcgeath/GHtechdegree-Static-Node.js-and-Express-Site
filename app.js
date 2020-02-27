87dbdfbb4e39fdf46ff0201eeafae1943a620ab8
Lucas McGeath <49741190+lmcgeath@users.noreply.github.com>
2020-02-27 13:36:56 -0800

    port changes

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
     next(err);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-   console.log('Application running on localhost:3000!')
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
 });
