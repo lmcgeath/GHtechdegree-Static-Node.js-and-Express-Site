e9ce755bc72ac85dad50d82930ddf9de4e69d09a
Lucas McGeath <49741190+lmcgeath@users.noreply.github.com>
2020-02-27 14:56:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -38,5 +38,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT ||3000, () => {
-    console.log('The application is running ' + process.env.PORT)
+    console.log('The application is running on localhost: ' + process.env.PORT)
 });
